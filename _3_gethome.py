319df3c418cade3f649d7d9d8eaa93c84b8a5093
root <tmeteorj@gmail.com>
2016-04-15 11:10:48 +0800

    resolve and add a solution for another type of data

--- _3_gethome.py
+++ _3_gethome.py
@@ -31,26 +31,17 @@ def loadbase(inputpath):
     for line in open(inputpath,"r"):
         info=line.strip().split(",")
         basemap[info[0]]=info[3]
-def gethome(recorddir,homepath):
+def gethomefromsinglefile(inputpath,user):
     global basemap
-    user=dict()
-    files=os.listdir(recorddir)
-    files.sort()
-    solvecnt=0
-    totalcnt=len(files)/2
-    starttime=time.time()
-    for name in files:
-        if name.find("traj.txt")==-1:continue
-        for line in open(recorddir+"/"+name,"r"):
-            #time,big-small,userid
-            info=line.strip().split(",")
-            hr=int(info[0][9:11])
-            if (hr>=21 or hr<7) and info[1] in basemap:
-                plane=basemap[info[1]]
-                if info[2] not in user:user[info[2]]=dict()
-                user[info[2]][plane]=1 if plane not in user[info[2]] else user[info[2]][plane]+1
-        solvecnt=solvecnt+1
-        outputinfo("gethome[%s]"%(recorddir),solvecnt,totalcnt,time.time()-starttime)
+    for line in open(inputpath,"r"):
+        #time,big-small,userid
+        info=line.strip().split(",")
+        hr=int(info[0][9:11])
+        if (hr>=21 or hr<7) and info[1] in basemap:
+            plane=basemap[info[1]]
+            if info[2] not in user:user[info[2]]=dict()
+            user[info[2]][plane]=1 if plane not in user[info[2]] else user[info[2]][plane]+1
+def outputuserhome(user,maxthred,outputpath):
     result=dict()
     for u in user:
         maxcnt=0
@@ -60,17 +51,26 @@ def gethome(recorddir,homepath):
             if cnt>maxcnt:
                 maxcnt=cnt
                 maxloc=pid
+        if maxcnt<maxthred:continue
         result[u+","+maxloc]=maxcnt
-    fw=open(homepath,"w")
+    fw=open(outputpath,"w")
     for x in sorted(result.items(),key=lambda arg:arg[1]):
         fw.write("%s,%d\n"%(x[0],x[1]))
     fw.close()
-def removeuser(homepath,realhome,thred):
-    fw=open(realhome,"w")
-    for line in open(homepath,"r"):
-        info=line.strip().split(",")
-        if int(info[-1])>=thred:fw.write("%s,%s\n"%(info[0],info[1]))
-    fw.close()
+def gethome(recorddir,homepath,maxthred):
+    global basemap
+    user=dict()
+    files=os.listdir(recorddir)
+    files.sort()
+    solvecnt=0
+    totalcnt=len(files)/2
+    starttime=time.time()
+    for name in files:
+        if name.find("traj.txt")==-1:continue
+        gethomefromsinglefile(recorddir+"/"+name,user)
+        solvecnt=solvecnt+1
+        outputinfo("gethome[%s]"%(recorddir),solvecnt,totalcnt,time.time()-starttime)
+    outputuserhome(user,maxthred,homepath)
 def decodeuser(usermappath,realhome,codehome):
     hashuser=dict()
     for line in open(usermappath,"r"):
@@ -81,12 +81,19 @@ def decodeuser(usermappath,realhome,codehome):
         info=line.strip().split(",")
         fw.write("%s,%s\n"%(hashuser[info[0]],info[1]))
     fw.close()
-if __name__=="__main__":
+def getanother():
     loadbase("BasePlaneSimple.csv")
     montharr=sys.argv[1:]
     for month in montharr:
-        gethome("i"+month,"home/userhome"+month+".txt")
-        print("Before remove and decode users")
-        removeuser("home/userhome"+month+".txt","home/realhome"+month+".txt",70)
-        decodeuser("hashuser/hash"+month+".txt","home/realhome"+month+".txt","home/originrealhome"+month+".txt")
-        print("After remove and decode users")
+        user=dict()
+        gethomefromsinglefile("i"+month+"/"+month+"traj.txt",user)
+        outputuserhome(user,70,"home/userhome"+month+".txt")
+        decodeuser("hashuser/hash"+month+".txt","home/userhome"+month+".txt","home/originrealhome"+month+".txt")
+def getall():
+    loadbase("BasePlaneSimple.csv")
+    montharr=sys.argv[1:]
+    for month in montharr:
+        gethome("i"+month,"home/userhome"+month+".txt",70)
+        decodeuser("hashuser/hash"+month+".txt","home/userhome"+month+".txt","home/originrealhome"+month+".txt")
+if __name__=="__main__":
+    getanother()
